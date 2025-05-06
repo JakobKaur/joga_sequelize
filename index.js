57c05a009b4e52751cdd54e4b81d35a25424b52f
Jakob Kaur <Jakob-kaur.kelder@voco.ee>
2025-05-06 11:33:37 +0300

    finished

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize("mysql://root:qwerty@localhost:3306/joga_sequelize");
-
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
 
 sequelize
     .authenticate()
     .then(() => {
-        console.log('Connected to the database.');
+        console.log("db connected")
     })
     .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+        console.error("db not connected: ", err)
+    })
 
-    
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to the application." });
-});
-
+const articleRouter = require('./routes/article')
+app.use('/', articleRouter)
+app.use('/article', articleRouter)
+app.use('/admin/article', articleRouter)
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+    console.log("running on http://localhost:3000")
+})
